d8095f22ad56771c647a6e1aa5a9213d37977cd9
mrvalty <merve.alty93@hotmail.com>
2023-05-30 21:58:17 +0300

    slayt resim ekleme

--- TeknoMuhendislikWebSite/Controllers/HomeController.cs
+++ TeknoMuhendislikWebSite/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace TeknoMuhendislikWebSite.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Index1()
+        {
+            return View();
+        }
     }
 }
